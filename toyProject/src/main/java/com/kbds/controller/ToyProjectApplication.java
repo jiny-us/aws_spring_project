7fd1eb51cd8e6ed6520ceba15921e94aaeae6823
sjh410 <sjh4010@gmail.com>
2020-08-11 10:37:10 +0900

    설정파일 수정

--- toyProject/src/main/java/com/kbds/controller/ToyProjectApplication.java
+++ toyProject/src/main/java/com/kbds/controller/ToyProjectApplication.java
@@ -2,12 +2,19 @@ package com.kbds.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToyProjectApplication {
+public class ToyProjectApplication extends SpringBootServletInitializer{
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ToyProjectApplication.class, args);
 	}
+	
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { return builder.sources(ToyProjectApplication.class); }
+
 
 }
